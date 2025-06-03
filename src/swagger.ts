20980de8f08bead698799c0da1d2e7df5f2c8a84
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-06-03 13:59:39 -0300

    install Swagger and create initial Swagger setup file

---
+++ src/swagger.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('JWT Auth API')
+    .setDescription('API with JWT authentication + refresh token')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
