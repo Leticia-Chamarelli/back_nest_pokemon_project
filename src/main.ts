8a1cbbbd6e9fb2235d7f06477ba5ad3f2aa38d79
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-06-03 14:03:52 -0300

    enable Swagger UI setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  setupSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
