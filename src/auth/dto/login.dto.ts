b861f779b08ae92d6462882e8d98821725d9a82e
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-06-03 14:20:57 -0300

    add Swagger documentation to LoginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,18 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({
+    example: 'testuser',
+    description: 'Username do usuário',
+  })
   @IsString()
   username: string;
 
+  @ApiProperty({
+    example: '123456',
+    description: 'Senha do usuário',
+  })
   @IsString()
   password: string;
 }
