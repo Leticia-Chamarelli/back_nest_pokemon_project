3eb659009ec10fdef08053a6dd81f4a3c7c871bc
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-06-14 16:19:53 -0300

    create endpoint to list captured Pokémon

--- src/pokemons/captured.controller.ts
+++ src/pokemons/captured.controller.ts
@@ -1,16 +1,21 @@
-import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { CapturedService } from './captured.service';
 import { CreateCapturedDto } from './dto/create-captured.dto';
+import { User } from 'src/users/user.entity';
 
 @Controller('captured')
+@UseGuards(JwtAuthGuard)
 export class CapturedController {
   constructor(private capturedService: CapturedService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post()
-  capture(@Body() dto: CreateCapturedDto, @Request() req) {
-    const userId = req.user.id;
-    return this.capturedService.capture(dto.pokemonId, userId);
+  capture(@Body() dto: CreateCapturedDto, @Request() req: { user: User }) {
+    return this.capturedService.capture(dto.pokemonId, req.user);
+  }
+
+  @Get()
+  findAll(@Request() req: { user: User }) {
+    return this.capturedService.findAllByUser(req.user);
   }
 }
--- src/pokemons/captured.service.ts
+++ src/pokemons/captured.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CapturedPokemon } from './captured-pokemon.entity';
 import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class CapturedService {
@@ -10,12 +11,20 @@ export class CapturedService {
     private capturedRepo: Repository<CapturedPokemon>,
   ) {}
 
-  async capture(pokemonId: number, userId: number) {
+  async capture(pokemonId: number, user: User) {
     const newCapture = this.capturedRepo.create({
       pokemonId,
-      user: { id: userId },
+      user,
     });
 
     return this.capturedRepo.save(newCapture);
+  }
+
+  async findAllByUser(user: User): Promise<CapturedPokemon[]> {
+    return this.capturedRepo.find({
+      where: { user: { id: user.id } },
+      relations: ['user'],
+      order: { capturedAt: 'DESC' },
+    });
   }
 }
