515a3bb19e01a31047f2585e9517bcc2a89c0bd0
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-06-23 15:38:33 -0300

    enable CORS for localhost:3000 frontend access

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { setupSwagger } from './swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.useGlobalFilters(new AllExceptionsFilter());
