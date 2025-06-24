5c5934468f421e24372496b38fd4255dba954ca3
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-06-24 18:37:08 -0300

    allow Authorization header and credentials for frontend requests

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,12 @@ import { setupSwagger } from './swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
+app.enableCors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
+
 
   app.useGlobalPipes(new ValidationPipe());
 
