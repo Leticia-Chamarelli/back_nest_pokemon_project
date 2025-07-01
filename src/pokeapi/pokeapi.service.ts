6f3c809f1f790f2c92f666e0be006c6e508a500d
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-07-01 13:15:09 -0300

    implement getPokemonDetails to fetch detailed data from PokéAPI

--- src/pokeapi/pokeapi.service.ts
+++ src/pokeapi/pokeapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { PokemonDetailDto } from 'src/pokemons/dto/pokemon-detail.dto';
 
 @Injectable()
 export class PokeapiService {
@@ -20,7 +21,6 @@ export class PokeapiService {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
     } catch (error: any) {
-    
       if (error?.response?.status === 404) {
         throw new NotFoundException(`Pokémon with id or name "${identifier}" not found.`);
       }
@@ -32,5 +32,35 @@ export class PokeapiService {
     const url = `${this.baseUrl}/pokemon?limit=${limit}&offset=${offset}`;
     const response = await firstValueFrom(this.httpService.get(url));
     return response.data;
+  }
+
+  async getPokemonDetails(pokemonId: number): Promise<PokemonDetailDto> {
+    const url = `${this.baseUrl}/pokemon/${pokemonId}`;
+    try {
+      const response = await firstValueFrom(this.httpService.get(url));
+      const data = response.data;
+
+      const detail: PokemonDetailDto = {
+        id: data.id,
+        name: data.name,
+        sprite: data.sprites.front_default,
+        types: data.types.map((t: any) => t.type.name),
+        abilities: data.abilities.map((a: any) => a.ability.name),
+        stats: data.stats.map((s: any) => ({
+          name: s.stat.name,
+          base_stat: s.base_stat,
+        })),
+        base_experience: data.base_experience,
+        height: data.height,
+        weight: data.weight,
+      };
+
+      return detail;
+    } catch (error: any) {
+      if (error?.response?.status === 404) {
+        throw new NotFoundException(`Pokémon with ID ${pokemonId} not found.`);
+      }
+      throw error;
+    }
   }
 }
--- src/pokemons/dto/pokemon-detail.dto.ts
+++ src/pokemons/dto/pokemon-detail.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PokemonDetailDto {
+   @ApiProperty()
+  id: number;
+
   @ApiProperty({ example: 'Pikachu' })
   name: string;
 
