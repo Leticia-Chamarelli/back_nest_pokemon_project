45c370e2d93808aae7721b975ef8122476b00980
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-07-01 16:07:34 -0300

    load environment config dynamically based on NODE_ENV (development or production)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development',
+});
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
@@ -7,17 +12,14 @@ import { setupSwagger } from './swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-app.enableCors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-});
-
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   app.useGlobalPipes(new ValidationPipe());
-
   app.useGlobalFilters(new AllExceptionsFilter());
-
   setupSwagger(app);
 
   await app.listen(process.env.PORT || 3000);
