d9e6f8b55cc5db33839e7d073176bd5a2eb862aa
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-07-01 13:09:28 -0300

    add PokemonDetailDto to structure PokéAPI response for detailed view

---
+++ src/pokemons/dto/pokemon-detail.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class PokemonDetailDto {
+  @ApiProperty({ example: 'Pikachu' })
+  name: string;
+
+  @ApiProperty({ example: 'https://raw.githubusercontent.com/...png' })
+  sprite: string;
+
+  @ApiProperty({ example: ['Electric'] })
+  types: string[];
+
+  @ApiProperty({ example: ['Static', 'Lightning Rod'] })
+  abilities: string[];
+
+  @ApiProperty({
+    example: [
+      { name: 'hp', value: 35 },
+      { name: 'attack', value: 55 },
+    ],
+  })
+  stats: { name: string; value: number }[];
+
+  @ApiProperty({ example: 112 })
+  base_experience: number;
+
+  @ApiProperty({ example: 4 })
+  height: number;
+
+  @ApiProperty({ example: 60 })
+  weight: number;
+}
